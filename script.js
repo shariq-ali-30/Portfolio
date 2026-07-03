1c34abd88f218cc0eb118751d9bfc85a17ffb67e
shariq-ali-30 <shariq3072007@gmail.com>
2026-07-03 15:26:31 +0500

    file upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let navLinks = document.querySelector(".nav-links").children
+
+function activeLinkHandler () {
+    
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].children[0].classList.remove("active")
+        this.children[0].classList.add("active")
+    }
+    
+    
+}
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", activeLinkHandler)
+}
